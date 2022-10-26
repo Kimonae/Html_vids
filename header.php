027574810f3abe9900b9d0b92e24dd31744e292e
Kimonae <shinokasu@gmail.com>
2022-10-26 22:43:37 +0200

    initial commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nesh </title>
+    <meta name="author" content="Saku">
+            <meta name="description" content="Nesh header">
+            <link rel="icon" href="icon.png"/>
+            <link rel="stylesheet" href="style.css"/>
+        </head>
+
+
+<body>
+    
+<img src ="aaa.jpg" id ="background"> 
+<h1> Free Nesh site </h1>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nesh </title>
+    <meta name="author" content="Saku">
+            <meta name="description" content="Nesh description haha">
+            <link rel="icon" href="icon.png"/>
+            <link rel="stylesheet" href="style.css"/>
+        </head>
+
+
+<body>
+
+<header> 
+    
+<?php 
+
+ include("header.php");
+
+?>
+
+
+</header>
+
+
+<h4> Infected </h4>
+
+
+<div class="video">
+<video controls="controls">
+<source src="aa.mp4" type="video/mp4">
+</video>
+</div>
+
+
+<h6> vids </h6>
+
+
+<footer> 
+
+<?php
+
+ include("footer.php");
+
+?>
+
+</footer>
+    
+</body>
+</html>
